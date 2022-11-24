fa32d682c8feebb5b783b0a91ff21664177fb8fb
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2022-11-24 06:58:16 +0100

    feat: add more posts

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ function App() {
             Create a post
           </button>
         </div>
-        <div className="mx-16 gap-4 my-8 text-black">
+        <div className="mx-16 gap-8 my-8 text-black flex flex-col">
           <PostCard
             authorImg={metamafia}
             authorName="MetaMafia.azero"
@@ -92,7 +92,52 @@ function App() {
             downVoteCount={21}
             text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
           />
-          <img src={lowerSeparator} alt="" className="m-auto mt-16" />
+          <PostCard
+            authorImg={metamafia}
+            authorName="MetaMafia.azero"
+            daoImg={DAO1}
+            daoName="AZERO TEST DAO"
+            tags={[
+              { color: "orange", label: "Governance" },
+              { color: "red", label: "DeFi" },
+              { color: "blue", label: "NFTs" },
+            ]}
+            time={Date.now() - 60 * 60 * 1.5}
+            upvoteCount={67}
+            downVoteCount={21}
+            text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          />
+          <PostCard
+            authorImg={metamafia}
+            authorName="MetaMafia.azero"
+            daoImg={DAO1}
+            daoName="AZERO TEST DAO"
+            tags={[
+              { color: "orange", label: "Governance" },
+              { color: "red", label: "DeFi" },
+              { color: "blue", label: "NFTs" },
+            ]}
+            time={Date.now() - 60 * 60 * 1.5}
+            upvoteCount={67}
+            downVoteCount={21}
+            text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          />
+          <PostCard
+            authorImg={metamafia}
+            authorName="MetaMafia.azero"
+            daoImg={DAO1}
+            daoName="AZERO TEST DAO"
+            tags={[
+              { color: "orange", label: "Governance" },
+              { color: "red", label: "DeFi" },
+              { color: "blue", label: "NFTs" },
+            ]}
+            time={Date.now() - 60 * 60 * 1.5}
+            upvoteCount={67}
+            downVoteCount={21}
+            text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+          />
+          <img src={lowerSeparator} alt="" className="m-auto mt-8" />
         </div>
       </div>
     </div>
