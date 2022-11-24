cf59c1cfe717295209e6b243927f753fc226ad59
ilovehackathons <106395723+ilovehackathons@users.noreply.github.com>
2022-11-24 02:38:01 +0100

    feat: implement an address dropdown

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { web3Enable } from "@polkadot/extension-dapp";
+import { web3Enable, web3Accounts } from "@polkadot/extension-dapp";
 import { useState } from "react";
 
 function App() {
   async function connect() {
     const allInjected = await web3Enable("Azero Debate");
     setWalletState(allInjected.length ? "connected" : "errored");
+    if (allInjected.length) {
+      setAccounts(await web3Accounts());
+    }
   }
   const [walletState, setWalletState] = useState("awaiting connection");
+  const [accounts, setAccounts] = useState([]);
   return (
-    <div className="p-4 flex items-center justify-between">
+    <div className="p-4 flex items-center justify-between gap-2">
       <img src={logo} alt="" />
       {walletState === "awaiting connection" ? (
         <button
@@ -20,12 +24,21 @@ function App() {
           Connect Wallet
         </button>
       ) : walletState === "connected" ? (
-        <button
-          className="mt-2 mr-2 rounded-3xl px-6 py-2 text-sm font-medium bg-teal-400 text-white"
-          onClick={connect}
-        >
-          Wallet Connected
-        </button>
+        <div className="flex flex-col gap-2">
+          <div>
+            <button
+              className="mt-2 mr-2 rounded-3xl px-6 py-2 text-sm font-medium bg-teal-400 text-white"
+              onClick={connect}
+            >
+              Wallet Connected
+            </button>
+          </div>
+          <select className="rounded-md border-2 border-teal-400 text-white w-40 bg-transparent">
+            {accounts.map((account) => (
+              <option key={account.address}>{account.address}</option>
+            ))}
+          </select>
+        </div>
       ) : (
         <button
           className="mt-2 mr-2 rounded-3xl px-6 py-2 text-sm font-medium bg-red-400 text-white"
