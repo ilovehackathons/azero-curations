26efb8af7cff274ece2f04521a88df068e3b39d6
mehdi <rhouzm@gmail.com>
2022-11-24 04:18:10 +0100

    feat : setup ink smart contracts

--- dao/target/debug/deps/dao-b54f1acb58e04ac8.d
+++
@@ -1,5 +0,0 @@
-/Users/mehdirhouzlane/AzeroDebate/dao/target/debug/deps/dao-b54f1acb58e04ac8.rmeta: src/main.rs
-
-/Users/mehdirhouzlane/AzeroDebate/dao/target/debug/deps/dao-b54f1acb58e04ac8.d: src/main.rs
-
-src/main.rs:
--- dao/target/debug/deps/dao-bd2987657627346c.d
+++
@@ -1,5 +0,0 @@
-/Users/mehdirhouzlane/AzeroDebate/dao/target/debug/deps/dao-bd2987657627346c.rmeta: src/main.rs
-
-/Users/mehdirhouzlane/AzeroDebate/dao/target/debug/deps/dao-bd2987657627346c.d: src/main.rs
-
-src/main.rs:
